3bdc2493762d72a37655690e7c53a2ef537512a4
amonov fozil <amonovfozilbek01@gmail.com>
2026-08-27 21:24:01 +0500

    deploy flutter web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "480203919" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "448375984" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
